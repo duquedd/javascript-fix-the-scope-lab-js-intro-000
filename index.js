12e36003ac770e13dcf1924ef0672458a918a536
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-14 21:23:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 funkyFunction()
 funkyFunction()
-var theFunk = funkyFunction(funkyFunction)
+var theFunk = funkyFunction()
